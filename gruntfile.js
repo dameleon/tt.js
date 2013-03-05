0f4b9ae1503216c09c7929e741542a58e7beb239
kei.takahashi <takahashi.kei@dena.jp>
2013-03-05 19:44:28 +0900

    use and run grunt

---
+++ gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        buster: {
+            test: {
+                config: 'test/buster.js',
+            },
+            server: {
+                port: 1111
+            }
+        },
+        concat: {
+            options: {
+                stripBanners: true,
+                banner: '/** <%= pkg.title %> version:<%= pkg.version %> author:<%= pkg.author.name %> at:<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            dist: {
+                src: 'src/*.js',
+                dest: '<%= pkg.name %>.all.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/** <%= pkg.title %> version:<%= pkg.version %> author:<%= pkg.author.name %> at:<%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: '<%= pkg.name %>.all.js',
+                dest: '<%= pkg.name %>.min.js'
+            }
+        },
+        jshint: {
+            all: 'src/*.js',
+            options: {
+                curly: true,
+                eqnull: true,
+                eqeqeq: true,
+                undef: true,
+                validthis: true,
+                globals: {
+                    tt: true
+                }
+            }
+        },
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-buster');
+
+    grunt.registerTask('test', ['jshint', 'buster']);
+    grunt.registerTask('default', ['jshint', 'buster', 'concat', 'uglify']);
+};
