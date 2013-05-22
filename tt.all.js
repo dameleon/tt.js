6b98d21cb8e313eb191b611be67c96241a94fc2b
Kei Takahashi <kei.takahashi@dena.com>
2013-05-22 16:47:08 +0900

    version++

--- tt.all.js
+++ tt.all.js
@@ -1,4 +1,4 @@
-/** tt.js version:1.0.2 author:kei takahashi(twitter@dameleon) at:2013-04-25 */
+/** tt.js version:1.0.3 author:kei takahashi(twitter@dameleon) at:2013-05-22 */
 ;(function(global, document, undefined) {
     "use strict";
 
@@ -152,6 +152,7 @@
         var args = tt_toArray(arguments),
             i = 1, iz = args.length,
             deep = false,
+            typeRe = /(object|array)/,
             arg, target;
 
         if (args[0] === true) {
@@ -162,18 +163,23 @@
 
         for (; i < iz; ++i) {
             arg = args[i];
-            if (tt_type(arg) !== "object") {
-                continue;
+            if (tt_type(arg, ["object", "array"])) {
+                tt_each(Object.keys(arg), _extend);
             }
-            tt_each(Object.keys(arg), _extend);
         }
         return target;
 
         function _extend(key, index) {
-            if (deep &&
-                tt_type(target[key], "object") &&
-                tt_type(arg[key], "object")) {
-                    tt_extend(target[key], arg[key]);
+            var argType = tt_type(arg[key]),
+                targetType = tt_type(target[key]);
+
+            if (deep && typeRe.test(argType)) {
+                if (argType === "object" && targetType === "object") {
+                    target[key] = {};
+                } else if (argType === "array" && targetType === "array") {
+                    target[key] = [];
+                }
+                tt_extend(deep, target[key], arg[key]);
             } else {
                 target[key] = arg[key];
             }
